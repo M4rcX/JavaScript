1e2bbfab50da49825f4ce201cd4ea43f22e74147
M4rcX <101703217+M4rcX@users.noreply.github.com>
2022-04-19 12:26:25 +0200

    Add files via upload

--- EjercicioAPI/app.js
+++ EjercicioAPI/app.js
@@ -1,105 +1,69 @@
 window.addEventListener("load", () =>{
-    //let longitud
-    //let latitud
 
-    //const ciudad = document.getElementById("ciudad");
-    //const formulario = document.getElementById("formulario")
+    let temperaturaValor = document.getElementById("temperatura-valor");
+    let temperaturaDescripcion = document.getElementById("temperatura-descripcion");
 
-    const temperaturaValor = document.getElementById("temperatura-valor");
-    const temperaturaDescripcion = document.getElementById("temperatura-descripcion");
+    let ubicacion = document.getElementById("ubicacion");
+    let iconoTiempo = document.getElementById("iconoTiempo");
 
-    const ubicacion = document.getElementById("ubicacion");
-    const iconoTiempo = document.getElementById("iconoTiempo");
+    let buscarCiudad = document.getElementById("ciudad");
+    let boton = document.getElementById("boton");
 
-    const velocidadViento = document.getElementById("velocidadViento");
+    let velocidadViento = document.getElementById("velocidadViento");
 
-     /*const api = {
-        clave: "e9ca141b3cdfec30cb5e1a1773d2e0a5",
-        url: "https://api.openweathermap.org/data/2.5/weather"
-    }
+    boton.addEventListener("click", function(){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+buscarCiudad.value+"&lang=es&units=metric&appid=e9ca141b3cdfec30cb5e1a1773d2e0a5";
+        fetch(url)
+            .then (Response => {return Response.json() })
+            .then (datos => {
 
-    async function buscar(query) {
-        try {
-            const response = await fetch (`${api.url}?q=${query}&appid=${api.clave}&lang=es`);
-            const data = await response.json();
-            console.log(data)
-        } catch (error) {
-            alert ("Hubo un error");
-        }
-    }
+                let temperatura = Math.round (datos.main.temp);
+                temperaturaValor.textContent = `${temperatura} ºC`;
 
-   function buscarCiudad (evento) {
-        evento.preventDefault();
-    }*/
+                let descripcion = datos.weather[0].description;
+                temperaturaDescripcion.textContent = descripcion.toUpperCase();
 
-    //formulario.addEventListener("submit", buscarCiudad, true);
-    
+                ubicacion.textContent = datos.name;
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition( posicion =>{
-                //longitud = posicion.coords.longitude
-                //latitud = posicion.coords.latitude
-
-                //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&exclude={part}&appid=e9ca141b3cdfec30cb5e1a1773d2e0a5`;
-                const url = `https://api.openweathermap.org/data/2.5/weather?q=barcelona&lang=es&units=metric&appid=e9ca141b3cdfec30cb5e1a1773d2e0a5`;
-
-               fetch(url)
-                    .then (Response => {return Response.json() })
-                    .then (datos => {
-
-                        let temperatura = Math.round (datos.main.temp)
-                        temperaturaValor.textContent = `${temperatura} ºC` 
-
-                        let descripcion = datos.weather[0].description
-                        temperaturaDescripcion.textContent = descripcion.toUpperCase()
-
-                        ubicacion.textContent = datos.name
-
-                        velocidadViento.textContent = `${datos.wind.speed} m/s`
-
-                        console.log(datos.weather[0].main)
-
-                        switch (datos.weather[0].main) {
-                            case "Clouds":
-                                iconoTiempo.src = "iconosAnimados/animated/cloudy-day-2.svg"
-                                console.log("Nublado")
+                velocidadViento.textContent = `${datos.wind.speed} m/s`;
+                buscarCiudad.value="";
+                
+                switch (datos.weather[0].main) {
+                    case "Clouds":
+                        iconoTiempo.src = "iconosAnimados/animated/cloudy-day-2.svg"
+                        console.log("Nublado")
+                         break;
+                    case "Thunderstorm":
+                        iconoTiempo.src = "iconosAnimados/animated/thunder.svg"
+                        console.log("Tormenta")
+                        break;
+                    case "Drizzle":
+                        iconoTiempo.src = "iconosAnimados/animated/rainy-2.svg"
+                        console.log("Llovizna")
+                        break;
+                    case "Rain":
+                        iconoTiempo.src = "iconosAnimados/animated/rainy-7.svg"
+                        console.log("Lluvia")
+                        break;
+                    case "Snow":
+                        iconoTiempo.src = "iconosAnimados/animated/snowy-3.svg"
+                        console.log("Nieve")
+                        break;
+                    case "Atmosphere":
+                        iconoTiempo.src = "iconosAnimados/animated/weather.svg"
+                        console.log("Atmosfera")
+                        break;
+                    case "Clear":
+                        iconoTiempo.src = "iconosAnimados/animated/day.svg"
+                        console.log("Despejado")
                                 break;
-                            case "Thunderstorm":
-                                iconoTiempo.src = "iconosAnimados/animated/thunder.svg"
-                                console.log("Tormenta")
-                                break;
-                            case "Drizzle":
-                                iconoTiempo.src = "iconosAnimados/animated/rainy-2.svg"
-                                console.log("Llovizna")
-                                break;
-                            case "Rain":
-                                iconoTiempo.src = "iconosAnimados/animated/rainy-7.svg"
-                                console.log("Lluvia")
-                                break;
-                            case "Snow":
-                                iconoTiempo.src = "iconosAnimados/animated/snowy-3.svg"
-                                console.log("Nieve")
-                                break;
-                            case "Atmosphere":
-                                iconoTiempo.src = "iconosAnimados/animated/weather.svg"
-                                console.log("Atmosfera")
-                                break;
-                            case "Clear":
-                                iconoTiempo.src = "iconosAnimados/animated/day.svg"
-                                console.log("Despejado")
-                                break;
-                                
-                        
-                            default:
-                                break;
+                    default:
+                        break;
                         }
-
-
-                    })
-                    .catch (error => {
-                        console.log(error)
-                    }) 
+            })
+            .catch (error => {
+                alert("HUBO UN ERROR, CORRIJA LA BÚSQUEDA")
+                console.log(error)
+            })
         })
-       
-    }
-})
+        })
